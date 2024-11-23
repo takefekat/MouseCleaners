1998df004ecdb950228d77abf97de6473c7a8eb2
takefekat <a.t.h.t.fumi@gmail.com>
2024-11-23 16:21:32 +0900

    iPadイベント設定バグ修正

--- ProcessiPad.py
+++ ProcessiPad.py
@@ -9,27 +9,27 @@ import json
 
 class ProcessiPad():
     def __init__(self, share_resouce:ShareResouce) -> None:
-        print("ProcessiPad.__init__")
+        print("[iPad   ]: ProcessiPad.__init__")
         self.share_resouce = share_resouce
         self._process_ipad = mp.Process(target=self.setup, name="iPadProcess")
 
     def setup(self):
-        print("ProcessiPad start")
+        print("[iPad   ]: ProcessiPad start")
 
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.bind(('192.168.251.3', 1250))  # IPとポート番号を指定します
+        self.s.bind(('192.168.251.3', 1251))  # IPとポート番号を指定します
         self.s.listen(5)
 
         while True:
             self.clientsocket, address = self.s.accept()
-            print(f"Connection from {address} has been established.")
+            print(f"[iPad   ]: Connection from {address} has been established.")
 
             # clientsocket が有効の場合、以下の処理を実行
             while self.clientsocket:
                 try:
                     # クライアントからのメッセージを受信
                     msg = self.clientsocket.recv(10000)
-                    print("rcv :", msg.decode("utf-8", errors="ignore"))
+                    print("[iPad   ]: rcv :", msg.decode("utf-8", errors="ignore"))
 
                     # msgをjson形式に変換
                     msg = msg.decode("utf-8", errors="ignore")
@@ -38,7 +38,7 @@ class ProcessiPad():
 
                         # "paths"が含まれている場合、パスを更新
                         if "paths" in msg_json:
-                            print("get paths from iPad")
+                            print("[iPad   ]: get paths from iPad")
                             for path in msg_json["paths"]:
                                 if path['mouse_id'] == 0:
                                     for i in range(len(path['path'])):
@@ -57,34 +57,34 @@ class ProcessiPad():
                                         self.share_resouce._path3[i] = path['path'][i]
                                     self.share_resouce._path3[len(path['path'])] = 255
                                 else:
-                                    print("mouse_id error")
+                                    print("[iPad   ]: mouse_id error")
+
+                                # 走行経路送信イベントをセット
+                                self.share_resouce._send_path_event[path['mouse_id']] = 1
 
                             # GUI更新イベントをセット
                             self.share_resouce._gui_update_event.set()
 
-                            # 走行経路送信イベントをセット
-                            for i in range(NUM_MOUSE):
-                                self.share_resouce._send_path_event[1] = 1
     
                         elif "signal" in msg_json:
                             if msg_json["signal"] == "start":
-                                print("start")
+                                print("[iPad   ]: start")
                                 # 走行開始イベントをセット
                                 for i in range(NUM_MOUSE):
                                   self.share_resouce._start_event[i] = 1
                             elif msg_json["signal"] == "stop":           
-                                print("stop")
+                                print("[iPad   ]: stop")
                                 # 走行停止イベントをセット
                                 for i in range(NUM_MOUSE):
                                     self.share_resouce._stop_event[i] = 1
 
                     except json.JSONDecodeError:
-                        print("Failed to decode JSON message")
+                        print("[iPad   ]: Failed to decode JSON message")
 
                     break # 一度受信したら終了
 
                 except BrokenPipeError:
-                    print("Connection closed by client.")
+                    print("[iPad   ]: Connection closed by client.")
                     self.clientsocket.close()
                     break
 
@@ -92,7 +92,7 @@ class ProcessiPad():
         self._process_ipad.start()
     
     def close(self):
-        print("ProcessiPad.close")
+        print("[iPad   ]: ProcessiPad.close")
         try:
             self.clientsocket.close()
             self.s.close()
